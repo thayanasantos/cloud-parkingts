de1b83b3c3ec8ca154dab164287bc39910df2999
thayanasantos <thayanasistemas@gmail.com>
2022-11-23 22:52:59 -0300

    Initial commit

---
+++ cloud-parkingts/src/main/java/one/digitalinnovation/parkingts/CloudParkingtsApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.parkingts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudParkingtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudParkingtsApplication.class, args);
+	}
+
+}
---
+++ cloud-parkingts/src/test/java/one/digitalinnovation/parkingts/CloudParkingtsApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.parkingts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
